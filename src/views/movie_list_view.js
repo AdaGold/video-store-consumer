2794a9009f93f0f272ce57b5dbcd7039d1a36785
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-12-18 12:41:17 -0800

    created the movie model and collection and views for each

---
+++ src/views/movie_list_view.js
@@ -0,0 +1,11 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import OrderView from '../views/movie_view';
+import Order from '../models/movie';
+
+const MovieListView = Backbone.View.extend({
+
+});
+
+
+export default MovieListView;
---
+++ src/views/movie_view.js
@@ -0,0 +1,8 @@
+import Backbone from 'backbone';
+import Movie from '../models/movie';
+
+const MovieView = Backbone.View.extend({
+
+});
+
+export default MovieView;
